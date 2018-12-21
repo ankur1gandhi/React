73c95eded151a823bbce13405612fba78f0fd53f
ankur1gandhi <ankur1gandhi@gmail.com>
2018-12-21 17:08:27 -0500

    added logic for todo list

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -5,10 +5,52 @@ import './App.css';
 import TodoList from './components/TodoList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: { text: '', key: '' }
+    };
+  }
+
+  // to handle data on input field on change
+  handleInput = e => {
+    const itemText = e.target.value;
+    const currentItem = { text: itemText, key: Date.now() };
+    this.setState({
+      currentItem
+    });
+  };
+
+  // to display the value of the state set to currentItem
+  currentItem = () => {
+    console.log('Hello Current Item');
+  };
+
+  // to handle click on add
+  addItem = e => {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== '') {
+      console.log(newItem);
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' }
+      });
+    }
+  };
+
+  // render
   render() {
     return (
       <div className="App">
-        <TodoList addItem={this.addItem} />
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.currentItem}
+        />
       </div>
     );
   }
--- react-todo/src/components/TodoList.js
+++ react-todo/src/components/TodoList.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+  // what happens on component update
+  componentDidUpdate() {
+    this.props.inputElement.current.focus();
+  }
+
+  // render
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
           <form onSubmit={this.props.addItem}>
-            <input placeholder="Enter task here..." />
+            <input
+              placeholder="Enter task here..."
+              ref={this.props.inputElement}
+              value={this.props.currentItem.text}
+              onChange={this.props.handleInput}
+            />
             <button type="submit"> Add Task </button>
           </form>
         </div>
